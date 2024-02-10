681816b8b5429a55d06c71352a7b6fac10ea361a
Ya-Pasha-3b4shy <pv-chernov2000@yandex.ru>
2024-02-10 23:47:08 +0300

    [CLibs][Linked List] two-way linked list updated

--- CLibs/linked_list/list.c
+++ CLibs/linked_list/list.c
@@ -24,27 +24,69 @@ static bool thisNodeAlreadyExistsByMemory(
   return false;
 }
 
-void listRemove(list_t list) {
-  list_node_t cursor_on_end = list->last_node;
-  for (int i = list->length; i > 0; i--) {
-    list_node_t tmp = cursor_on_end->prev;
-    free(cursor_on_end);
-    cursor_on_end = tmp;
+static list_node_t getNodeByIndex(list_t list, size_t index) {
+  size_t i = 0;
+  FOREACH_NODE_FROM_BEGIN(i, list) {
+    if (i == index) {
+      return node;
+    }
   }
+  return NULL;
+}
+
+static bool lessThanPivot(list_node_t pivot_obj, list_node_t current_obj) {
+  return current_obj->payload < pivot_obj->payload; 
+}
+
+static bool greaterThanPivot(list_node_t pivot_obj, list_node_t current_obj) {
+  return current_obj->payload > pivot_obj->payload;
+}
+
+typedef bool (*comparatorWithPivot)(list_node_t, list_node_t);
+
+static list_t createListByCondition(list_t list, list_node_t pivot_node,
+                                    comparatorWithPivot cmp) {
+  size_t iterator;
+  list_t selected_list = listInit(0);
+  FOREACH_NODE_FROM_BEGIN(iterator, list) {
+    if (cmp(pivot_node, node)) {
+      selected_list->listInsertAtEnd(selected_list, node);
+    }
+  }
+  if (selected_list->length == 0) {
+    free(selected_list);
+    return NULL;
+  }
+}
+
+void listRemove(list_t list) {
+  size_t iterator;
+  list_node_t tmp_cursor;
+  FOREACH_NODE_FROM_END_WITHOUT_TRANSITION(iterator, list) {
+    tmp_cursor = node->prev;
+    free(node);
+    // manual transition
+    node = tmp_cursor;
+  }
+  list->last_node = NULL;
+  list->first_node = NULL;
+
   free(list);
 }
 
 list_t listInit(size_t size) {
-  if (!size) {
-    return NULL;
-  }
   list_t list = malloc(sizeof(list_s));
   if (!list) {
     return NULL;
   }
+  if (!size) {
+    list->length = 0;
+    list->last_node = NULL;
+    list->first_node = NULL;
+    return list;
+  }
 
   list->length = size;
-
   list->first_node = malloc(sizeof(list_node_s));
   if (!list->first_node) {
     return NULL;
@@ -59,7 +101,7 @@ list_t listInit(size_t size) {
 
     tmp->prev = cursor_on_previous;
     cursor_on_previous->next = tmp;
-    
+
     cursor_on_previous = tmp;
   }
   list->last_node = cursor_on_previous;
@@ -68,37 +110,57 @@ list_t listInit(size_t size) {
   return list;
 }
 
-void listInsertAtEnd(list_node_t node, list_t list) {
+void listInsertAtEnd(list_t list, list_node_t node) {
   if (thisNodeAlreadyExistsByMemory(node, list)) {
     return;
   }
   list->length++;
+  if (list->last_node == NULL && list->first_node == NULL) {
+    list->last_node = node;
+    list->first_node = node;
+    
+    list->first_node->next = list->last_node;
+    list->first_node->prev = list->last_node;
 
-  list->last_node->next = node;
-  list->first_node->prev = node;
+    list->last_node->prev = list->first_node;
+    list->last_node->next = list->first_node;
+  } else {
+    list->last_node->next = node;
+    list->first_node->prev = node;
 
-  node->prev = list->last_node;
-  node->next = list->first_node;
+    node->prev = list->last_node;
+    node->next = list->first_node;
 
-  list->last_node = node;
+    list->last_node = node;
+  }
 }
 
-void listInsertAtBegin(list_node_t node, list_t list) {
+void listInsertAtBegin(list_t list, list_node_t node)  {
   if (thisNodeAlreadyExistsByMemory(node, list)) {
     return;
   }
   list->length++;
+  if (list->last_node == NULL && list->first_node == NULL) {
+    list->last_node = node;
+    list->first_node = node;
+    
+    list->first_node->next = list->last_node;
+    list->first_node->prev = list->last_node;
 
-  list->last_node->next = node;
-  list->first_node->prev = node;
+    list->last_node->prev = list->first_node;
+    list->last_node->next = list->first_node;
+  } else {
+    list->last_node->next = node;
+    list->first_node->prev = node;
 
-  node->prev = list->last_node;
-  node->next = list->first_node;
+    node->prev = list->last_node;
+    node->next = list->first_node;
 
-  list->first_node = node;
+    list->first_node = node;
+  }
 }
 
-void listInsert(size_t idx, list_node_t node, list_t list) {
+void listInsert(list_t list,  list_node_t node, size_t idx) {
   if (idx >= list->length) {
     return;
   }
@@ -134,6 +196,61 @@ void listInsert(size_t idx, list_node_t node, list_t list) {
     list->first_node = node;
   }
   list->length++;
+}
+
+void listErase(list_t list) {
+  size_t iterator;
+  FOREACH_NODE_FROM_BEGIN(iterator, list) {
+    node->payload = 0;
+  }
+}
+
+void listRemoveAllNodes(list_t list) {
+  size_t iterator;
+  list_node_t tmp_cursor;
+  FOREACH_NODE_FROM_END_WITHOUT_TRANSITION(iterator, list) {
+    tmp_cursor = node->prev;
+    free(node);
+    // manual transition
+    node = tmp_cursor;
+  }
+  list->last_node = NULL;
+  list->first_node = NULL;
+}
+
+void listRemoveNodeByIndex(list_t list, size_t index) {
+  if (index >= list->length) {
+    return;
+  }
+
+  size_t iterator; 
+  list_node_t node_to_remove = NULL;
+  FOREACH_NODE_FROM_BEGIN(iterator, list) {
+    if (iterator == index) {
+      node_to_remove = node;
+      break;
+    }
+  }
+  if (node_to_remove == NULL) {
+    return;
+  }
+  node_to_remove->prev->next = node_to_remove->next;
+  node_to_remove->next->prev = node_to_remove->prev;
+  free(node_to_remove);
+}
+
+/* algorithms for linked list */
+
+void listSort(list_t list) {
+  list_node_t pivot_node = getNodeByIndex(list, list->length / 2);
+  list_t less_pivot = createListByCondition(list, pivot_node,
+                                            &lessThanPivot);
+  list_t greater_pivot = createListByCondition(list, pivot_node,
+                                               &greaterThanPivot);
+  // TODO(k1rch): merge lists with free(less_pivot) or listSort(greater_pivot)
+  //              depending on lenght of lists. Удалить сами списки, а элементы
+  //              элегантно перенести в новый список. Это можно сделать за O(1)
+  // return mergeLists(listSort(less_pivot), pivot_node, listSort(greater_pivot));
 }
 
 void listPrint(list_t list) {
@@ -182,11 +299,27 @@ int listBsearch(list_t list, int number) {
 }
 */
 
-int main(void) {
-  size_t len;
-  printf("Get lenght of list: "); scanf("%ld", &len);
+list_t List(size_t nmemb) {
+  list_t this = listInit(nmemb);
 
-  list_t lst = listInit(len);
+  this->listRemove = &listRemove;
+
+  this->listInsert = &listInsert;
+  this->listInsertAtEnd = &listInsertAtEnd;
+  this->listInsertAtBegin = &listInsertAtBegin;
+
+  // this->listSort = &listSort;
+  // this->listErase = &listErase;
+
+  return this;
+}
+
+int main(void) {
+  size_t nmemb;
+  printf("Get lenght of list: "); scanf("%ld", &nmemb);
+
+  list_t lst = List(nmemb);
+
   if (lst == NULL)
     return EXIT_SUCCESS;
 
@@ -194,35 +327,28 @@ int main(void) {
 
   list_node_t item0 = calloc(1, sizeof(list_node_s));
   item0->payload = 111;
-  listInsertAtBegin(item0, lst);
+  lst->listInsertAtBegin(lst, item0);
 
-  // list_node_t item1 = calloc(1, sizeof(list_node_s));
-  // item1->payload = 222;
-  // listInsertAtEnd(item1, lst);
-
-  // listPrint(lst);
-
-  // list_node_t item2 = calloc(1, sizeof(list_node_s));
-  // item2->payload = 333;
-  // listInsert(lst->length / 2, item2, lst);
+  list_node_t item1 = calloc(1, sizeof(list_node_s));
+  item1->payload = 222;
+  lst->listInsertAtEnd(lst, item1);
 
   // listPrint(lst);
-  // listReverseByPayloads(lst);
+
+  list_node_t item2 = calloc(1, sizeof(list_node_s));
+  item2->payload = 333;
+  listInsert(lst, item2, lst->length / 2);
+
+  listPrint(lst);
+  listReverseByPayloads(lst);
   // listPrint(lst);
 
-  size_t i = 0;
+  size_t i;
   FOREACH_NODE_FROM_BEGIN(i, lst) {
     printf("%d \n", node->payload);
   }
-  printf("\n");
 
-  i = 0;
-  FOREACH_NODE_FROM_END(i, lst) {
-    printf("%d\n", node->payload);
-  }
-
-  listRemove(lst);
-
+  lst->listRemove(lst);
 
   return EXIT_SUCCESS;  
 } 
--- CLibs/linked_list/list.h
+++ CLibs/linked_list/list.h
@@ -4,6 +4,48 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+/** @brief
+ * Список обладает быстрой вставкой
+ * и быстрым удалением элементов
+ * с начала и с конца - O(1). 
+ * 
+ * Чтение занимает время O(n),
+ * где n - количество элементов списка.
+ * Количество итераций по списку = O(n). 
+*/
+
+struct list;
+struct list_node;
+
+typedef struct list* list_t;
+typedef struct list_node* list_node_t;
+
+/* operations with memory */
+
+void listRemove(list_t list);
+list_t listInit(size_t lst_size);
+
+void listInsertAtEnd(list_t list, list_node_t node);
+void listInsertAtBegin(list_t list, list_node_t node);
+void listInsert(list_t list,  list_node_t node, size_t idx);
+
+void listErase(list_t list); 
+void listRemoveAllNodes(list_t list);
+void listRemoveNodeByIndex(list_t list, size_t index);
+
+/* algorithms for linked list */
+
+void listSort(list_t list);
+void listPrint(list_t list);
+void listReverseByPayloads(list_t list);
+int listBsearch(list_t list, int number);
+
+/* Calls for initialize a two-way list */
+
+list_t List(size_t nmemb);
+
+/* defining of list and list_node by structs */
+
 typedef struct list_node {
   struct list_node* next;
   struct list_node* prev;
@@ -18,38 +60,36 @@ typedef struct list {
 
   list_node_t last_node;
   list_node_t first_node;
+
+  void (*listRemove)(struct list*);
+
+  void (*listInsertAtEnd)(struct list* list, list_node_t node);
+  void (*listInsertAtBegin)(struct list* list, list_node_t node);
+  void (*listInsert)(struct list* list, list_node_t node, size_t idx);
+
+  void (*listSort)(struct list* list);
+  void (*listErase)(struct list* list);
 } list_s;
 
 typedef list_s* list_t;
 
-/* operations with memory */
-
-void listRemove(list_t list);
-list_t listInit(size_t lst_size);
-
-void listInsertAtEnd(list_node_t node, list_t list);
-void listInsertAtBegin(list_node_t node, list_t list);
-void listInsert(size_t idx, list_node_t node, list_t list);
-
-void listSort(list_t list);
-void listErase(list_t list); 
-
-/* algorithms for list */
-
-void listPrint(list_t list);
-void listReverseByPayloads(list_t list);
-int listBsearch(list_t list, int number);
-
 /* Macroses foreach */
 
 #define FOREACH_NODE_FROM_END(i, list) \
+  i = 0; \
   for (list_node_t node = list->last_node; \
-       i < list->length; \
-       i++, node = node->prev)
+    i < list->length; \
+    ++i, node = node->prev)
+
+#define FOREACH_NODE_FROM_END_WITHOUT_TRANSITION(i, list) \
+  i = list->length; \
+  for (list_node_t node = list->last_node; \
+       i > 0; --i)
 
 #define FOREACH_NODE_FROM_BEGIN(i, list) \
+  i = 0; \
   for (list_node_t node = list->first_node; \
-       i < list->length; \
-       i++, node = node->next)
+    i < list->length; \
+    ++i, node = node->next)
 
 #endif // LIST_LIBRARY_H
